a2e31d8de34a473966e4987f7b4a1dcccdd0daa8
Antonio Caiazzo <antoniocaiazzo2003@gmail.com>
2026-08-22 23:37:09 +0200

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        def is_palindrome(l, r):
+            return s[l:r + 1] == s[l:r + 1:][::-1]
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+
+            if s[l] != s[r]:
+                return is_palindrome(l + 1, r) or is_palindrome(l, r - 1)
+            else:
+                l += 1
+                r -= 1
+        return True
