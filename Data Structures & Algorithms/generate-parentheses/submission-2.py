6c255c998324a017e799664605ab1b4fd9abe341
Antonio Caiazzo <antoniocaiazzo2003@gmail.com>
2026-09-05 10:20:48 +0200

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        resultComb, currComb = [], []
+
+        def backtracking(open_brackets, close_brackets):
+
+            if open_brackets == close_brackets and open_brackets == n:
+                resultComb.append("".join(currComb))
+                return
+
+            
+            if open_brackets < n:
+                currComb.append("(")
+                backtracking(open_brackets + 1, close_brackets)
+                currComb.pop()
+
+            if close_brackets < open_brackets:
+                currComb.append(")")
+                backtracking(open_brackets, close_brackets + 1)
+                currComb.pop()
+
+
+        backtracking(0, 0)
+        return resultComb
+        
