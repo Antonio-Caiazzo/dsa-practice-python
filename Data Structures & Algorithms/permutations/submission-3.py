06b536dcab334dc4d3c5a49aba3a2ba181ebcf82
Antonio Caiazzo <antoniocaiazzo2003@gmail.com>
2026-09-07 11:50:35 +0200

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+
+        for n in nums:
+            new_perm = []
+            for p in result:
+                for j in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(j, n)
+                    new_perm.append(pCopy)
+                result = new_perm
+        return result
